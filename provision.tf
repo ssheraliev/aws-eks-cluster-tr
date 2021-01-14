866adec4a71daf311a76666b6f7ea696eb8abd55
ssheraliev <ssheraliev@outlook.com>
2021-01-14 10:23:10 +0000

    Terraform file commit to GitHub repo

---
+++ provision.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "Enter Access key value"
+  secret_key = "Enter Secret key value"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+data "aws_availability_zones" "available" {
+}
+
+locals {
+  cluster_name = "ENTER Cluster name"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.58"
+
+name                 = "k8s-cluster-vpc-001"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "12.2.0"
+
+cluster_name    = "ENTER Cluster name"
+  cluster_version = "1.18"
+  subnets         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  node_groups = {
+    first = {
+      desired_capacity = 3
+      max_capacity     = 3
+      min_capacity     = 5
+
+      instance_type = "t2.micro"
+    }
+  }
+
+  write_kubeconfig   = true
+  config_output_path = "./"
+}
